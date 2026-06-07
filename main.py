678a6b06bb727f4979e75411f1fff06340fafbe8
428937 <alpkaanayas@gmail.com>
2026-06-08 00:11:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ model += (
 for c in cities:
     model += lpSum(shipment[(w, c)] for w in warehouses) == demand[c]
 
-# Warehouse capacity constraint
+# Warehouse capacity constraints
 # If warehouse is closed, allowed shipment becomes zero
 for w in warehouses:
     model += (
